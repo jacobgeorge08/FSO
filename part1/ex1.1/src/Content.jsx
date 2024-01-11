f9c81d8d76e1e26bfdb4f95d68451181232818ab
Jacob <jacobgeorge08@gmail.com>
2024-01-11 16:29:02 +0530

    ex 1.4 turned parts into an array of objects

--- part1/ex1.1/src/Content.jsx
+++ part1/ex1.1/src/Content.jsx
@@ -3,9 +3,9 @@ import Part from "./Part";
 const Content = (props) => {
   return (
     <div>
-      <Part part={props.part1} ex={props.ex1} />
-      <Part part={props.part2} ex={props.ex2} />
-      <Part part={props.part3} ex={props.ex3} />
+      <Part part={props.parts[0].name} ex={props.parts[0].exercises} />
+      <Part part={props.parts[1].name} ex={props.parts[1].exercises} />
+      <Part part={props.parts[2].name} ex={props.parts[2].exercises} />
     </div>
   );
 };
