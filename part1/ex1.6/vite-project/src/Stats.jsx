75fa03327a05be78d3a6be91d9f35b2ea3ee71fc
Jacob <jacobgeorge08@gmail.com>
2024-01-15 07:39:32 +0530

    unicafe 1.7 done

--- part1/ex1.6/vite-project/src/Stats.jsx
+++ part1/ex1.6/vite-project/src/Stats.jsx
@@ -1,10 +1,15 @@
 const Stats = ({ good, bad, neutral }) => {
+  const total = good + bad + neutral;
+  const average = (good - bad) / total;
   return (
     <div>
       <span>Statistics</span>
       <div>good {good}</div>
       <div>bad {bad}</div>
       <div>neutral {neutral}</div>
+      <div>all {total}</div>
+      <div>average {average}</div>
+      <div>positive {(good / total) * 100} %</div>
     </div>
   );
 };
