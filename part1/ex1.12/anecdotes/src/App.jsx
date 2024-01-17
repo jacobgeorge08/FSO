9ce2a569e2a829df78d0ec71bfb9238e758949cf
Jacob <jacobgeorge08@gmail.com>
2024-01-17 14:58:50 +0530

    completed part1

---
+++ part1/ex1.12/anecdotes/src/App.jsx
@@ -0,0 +1,71 @@
+import { useState } from "react";
+
+const Button = ({ handleClick, text }) => {
+  return <button onClick={handleClick}>{text}</button>;
+};
+
+const Quote = ({ header, quote, points }) => {
+  let maxVotes = points.reduce((acc, val, i) => {
+    if (val > points[acc]) {
+      acc = i;
+    }
+    return acc;
+  }, 0);
+
+  let bool = points.every((num) => num == 0);
+  if (bool) {
+    return <p>No Votes Cast Yet</p>;
+  } else {
+    return (
+      <>
+        <h2>{header}</h2>
+        <p>{quote[maxVotes]}</p>
+        <p>has {points[maxVotes]} votes</p>
+      </>
+    );
+  }
+};
+
+const App = () => {
+  const anecdotes = [
+    "If it hurts, do it more often.",
+    "Adding manpower to a late software project makes it later!",
+    "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    "Premature optimization is the root of all evil.",
+    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
+    "The only way to go fast, is to go well.",
+  ];
+
+  const [points, setPoints] = useState(new Array(anecdotes.length).fill(0));
+  const [selected, setSelected] = useState(0);
+
+  const handleClick = () => {
+    const rAnecdote = Math.floor(Math.random() * anecdotes.length);
+    setSelected(rAnecdote);
+  };
+
+  const handleVote = () => {
+    const incrementPoints = [...points];
+    incrementPoints[selected] += 1;
+    setPoints(incrementPoints);
+  };
+
+  return (
+    <div>
+      <h2>Anecdote of the Day</h2>
+      <p>{anecdotes[selected]}</p>
+      <p>has {points[selected]} votes</p>
+      <Button handleClick={handleVote} text="vote" />
+      <Button handleClick={handleClick} text="next anecdote" />
+      <Quote
+        header="Anecdotes with the most votes"
+        points={points}
+        quote={anecdotes}
+      />
+    </div>
+  );
+};
+
+export default App;
