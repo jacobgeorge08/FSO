0d7ba5674192c488b6ebe92b974ef2eb741f595f
Jacob <jacobgeorge08@gmail.com>
2024-01-22 13:17:48 +0530

    completed 2.8

--- part2/ex2.6/src/App.jsx
+++ part2/ex2.6/src/App.jsx
@@ -2,30 +2,57 @@ import { useState } from "react";
 
 const Persons = ({ persons }) => {
   const mappedPersons = persons.map((p) => {
-    return <p key={p.name}>{p.name}</p>;
+    return (
+      <div key={p.name}>
+        {p.name} {p.number}
+      </div>
+    );
   });
   return <>{mappedPersons}</>;
 };
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "007" },
+  ]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
 
-  const handleChange = (event) => {
+  const handleNameChange = (event) => {
     console.log(event.target.value);
     setNewName(event.target.value);
   };
+
+  const handleNumberChange = (event) => {
+    console.log(event.target.value);
+    setNewNumber(event.target.value);
+  };
+
   const handleSubmit = (event) => {
     event.preventDefault();
-    setPersons([...persons, { name: newName }]);
+
+    const found = persons.find((p) => {
+      return p.name === newName && p.number === newNumber;
+    });
+
+    if (found) {
+      alert(`${newName} already exists in the phone book`);
+    } else {
+      setPersons([...persons, { name: newName, number: newNumber }]);
+    }
   };
+
   console.log(persons);
+
   return (
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={handleSubmit}>
         <div>
-          name: <input value={newName} onChange={handleChange} />
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
         </div>
         <div>
           <button type="submit">add</button>
@@ -33,7 +60,9 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <Persons persons={persons} />
-      <div>debug: {newName}</div>
+      <div>
+        DEBUG: Name {newName} Number {newNumber}
+      </div>
     </div>
   );
 };
