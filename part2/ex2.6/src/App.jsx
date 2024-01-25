5bfba4de0c9dd40c2f84f7f1d7b612f8f592b8f2
Jacob <jacobgeorge08@gmail.com>
2024-01-25 15:59:55 +0530

    moved initial person list to db.json. completed ex2.11

--- part2/ex2.6/src/App.jsx
+++ part2/ex2.6/src/App.jsx
@@ -3,16 +3,24 @@ import PersonForm from "./PersonForm";
 import Persons from "./Persons";
 import Filter from "./Filter";
 
+import axios from "axios";
+import { useEffect } from "react";
+
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [search, setSearch] = useState("");
+
+  const hook = () => {
+    console.log("effect");
+    axios.get("http://localhost:3001/persons").then((res) => {
+      console.log("Promise fulfilled");
+      setPersons(res.data);
+    });
+  };
+
+  useEffect(hook, []);
 
   const handleNameChange = (event) => {
     setNewName(event.target.value);
